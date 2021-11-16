049d7c6b7303b7c0a19392c7e426372966f5ce95
Kalle-Lassila <kalle.lassila@protonmail.com>
2021-11-16 22:15:21 +0200

    Sending commands to the remote interface now works and provided examples.

--- control_software/socket_controller.py
+++ control_software/socket_controller.py
@@ -1,17 +1,17 @@
 #socket_controller module for handling comms with ur16e and ur10 robots
-#Oulu University of Applied Sci.ences
+#Oulu University of Applied Sciences
 #TVT19SPL
-import socket, threading
+#Currently robot needs to be in local control mode and the following function must be running in the robot
+#interpreter_mode(clearQueueOnEnter=False)
+import socket, threading, time
 
 class RobotConnectionManager():
     '''Creates socket for communicating with the robot and handles recieved TCP messages'''
     def __init__(self, port: int, buffer: int):
         self.robot_port = port  #port used to communicate with the ur control box
-        #TODO logic for determining message size dynamically, must also probably be implemented on the robot side
-        #messages are recieved one at a time anyways so just using a big enough buffer to chatch all is prob enough
         self.buffer_size = buffer   #size of the message buffer used(the number of bytes in each message sent by the robot)
         
-    def connection(self):
+    def server_connection(self):
         '''Starts and in the future manages socket connection to the robot or any socket for that matter'''
         #TODO unexpected disconnections are not handled at all
         self.base_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #initialize socket object
@@ -19,17 +19,17 @@ class RobotConnectionManager():
         self.base_socket.listen(5) #start listening and allow 5 failed attempts to connect
         self.robot_socket, self.robot_address = self.base_socket.accept()
         while True:
-            #TODO recive data and send data prob need their own threads or async methods
             message = self.robot_socket.recv(self.buffer_size)  #get buffer_size of bytes from socket buffer
             message = message.decode("utf-8")   #decode bytes to utf-8 character string
             print(message)
 
     def client_connection(self):
-        self.robot_address = "172.16.177.128"
         self.robot_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.robot_socket.connect((self.robot_address, self.robot_port))
         while True:
-            message = self.robot_socket.recv(self.buffer_size)
+            #TODO This resets user input with each print... Fix it
+            time.sleep(1)
+            #message = self.robot_socket.recv(self.buffer_size)
             #message = message.decode("utf-8")
             #print(message)
     
@@ -37,28 +37,29 @@ class RobotConnectionManager():
         '''Take user input and sen it to socket'''
         #TODO command messages should only be sent when the robot is ready to recieve new instructions
         while True:
-            message = 'popup("Hi")'
-            gg = input()
+            message = input()   #Commands can be entered as in "popup("Hi")"
+            message += "\n" #append newline to the end to avoid always typing it
             self.robot_socket.send(bytes(message,"utf-8"))  #use the socket created in rcm to send string as bytes
     
-    def start_client(self):
-        client_thread = threading.Thread(target=self.client_connection)
+    def begin(self, mode: str):
+        '''Automatically creates needed threads so user does not need to worry about these.
+        possible options for mode are: "server" and "client"'''
+        if mode == "client":
+            connection_thread = threading.Thread(target=self.client_connection)
+        elif mode == "server":
+            connection_thread = threading.Thread(target=self.server_connection)
+
         send_thread = threading.Thread(target=self.send, daemon=True)
         send_thread.start()
-        client_thread.start()
-        
-    def start_server(self):
-        '''Automatically creates needed threads so user does not need to worry about these'''
-        send_thread = threading.Thread(target=self.send, daemon=True)   #create thread and put it to background
-        socket_thread = threading.Thread(target=self.connection)
-        send_thread.start()
-        socket_thread.start()
+        connection_thread.start()
 
 class Main():
     @staticmethod
     def main():
-        rcm = RobotConnectionManager(29999, 4096)#30001, 256)  #takes port number and buffer size as inputs for init
-        rcm.start_client()
+        rcm = RobotConnectionManager(30020, 4096)   #takes port number and buffer size as inputs for init
+        rcm.robot_address = "172.16.140.130"    #ip of the robot, needed in client mode
+        rcm.begin("client")  #start rcm in client mode
+        #rcm.start_server()  #start rcm in server mode
         
 if __name__ == "__main__":
     c = Main()
