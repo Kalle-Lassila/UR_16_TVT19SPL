4096420b141d8eff9f9afa7f41ced0f9b3766e4f
Kalle-Lassila <kalle.lassila@protonmail.com>
2021-11-19 15:20:57 +0200

    sorta working example with tkinter

--- control_software/tkinter_test.py
+++ control_software/tkinter_test.py
@@ -1,33 +1,23 @@
 import tkinter
-from tkinter.constants import BOTTOM
+from tkinter import scrolledtext
+from tkinter.constants import INSERT
+window = tkinter.Tk()
+window.title("Scrolling terminal")
+output_box = scrolledtext.ScrolledText(window, wrap = tkinter.WORD, width = 41, height = 10, font = ("Times New Roman", 12))
 
-root = tkinter.Tk()
-root.title("Hello GUI!")
-root.geometry('320x200')
+input_box = tkinter.Entry(width=40, font=("Times New Roman", 12))
 
-lbl = tkinter.Label(root, text="This is a label")
-lbl.grid()
-txt = tkinter.Entry(root, width=10)
-txt.grid(column=0,row=1)
-def click():
-    lbl.configure(text="bork")
-
-btn = tkinter.Button(root, text = "click me", fg="red", command=click)
-btn.grid(column=1, row=0)
-
+def handler(something):
+    input = input_box.get()
+    output_box.configure(state=tkinter.NORMAL)
+    input_box.delete(0,tkinter.END)
+    output_box.insert(INSERT, input+"\n")
+    output_box.configure(state=tkinter.DISABLED)
 
 
+output_box.grid(column=0, row=10)
+output_box.configure(state="disabled")
+input_box.grid(column=0,row=11)
 
-
-
-
-
-
-
-
-
-
-
-
-
-root.mainloop()
+window.bind('<Return>', handler)
+window.mainloop()
