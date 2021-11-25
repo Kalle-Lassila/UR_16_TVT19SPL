64270d559aee0fc27e6083edea3b31e1ed5fca8b
Kalle Lassila <kalle.lassila@protonmail.com>
2021-11-25 16:50:28 +0200

    Added port and ip options in GUI
    Also broke stuffs, app freezes if there is no incoming traffic.

--- control_software/RobotConnectionManager.py
+++ control_software/RobotConnectionManager.py
@@ -5,58 +5,56 @@
     #Currently robot needs to be in local control mode and the following function must be running in the robot
     #interpreter_mode(clearQueueOnEnter=False)
 
-import socket, threading, time
+import socket, threading
 
 class RobotConnectionManager():
     '''Creates socket for communicating with the robot and handles recieved TCP messages'''
-    def __init__(self, port: int, buffer: int):
-        self.robot_port = port  #port used to communicate with the ur control box
+    def __init__(self, ip: str, port: int, buffer: int):
+        self.robot_address = ip
+        self.__robot_port = port  #port used to communicate with the ur control box
         self.buffer_size = buffer   #size of the message buffer used(the number of bytes in each message sent by the robot)
+                                    #Can in theory be modified on the fly
         
-    def server_connection(self):
+    def __server_connection(self):
         '''Starts and in the future manages socket connection to the robot or any socket for that matter'''
         #TODO unexpected disconnections are not handled at all
-        self.base_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #initialize socket object
-        self.base_socket.bind(("0.0.0.0", self.robot_port))    #bind the socket to unroutable address for now, and listen to robot_port
-        self.base_socket.listen(5) #start listening and allow 5 failed attempts to connect
-        self.robot_socket, self.robot_address = self.base_socket.accept()
+        self.__base_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #initialize socket object
+        self.__base_socket.bind(("0.0.0.0", self.__robot_port))    #bind the socket to unroutable address for now, and listen to __robot_port
+        self.__base_socket.listen(5) #start listening and allow 5 failed attempts to connect
+        self.__robot_socket, self.robot_address = self.__base_socket.accept()   #accept inbound connection
         while True:
-            message = self.robot_socket.recv(self.buffer_size)  #get buffer_size of bytes from socket buffer
+            message = self.__robot_socket.recv(self.buffer_size)  #get buffer_size of bytes from socket buffer
             message = message.decode("utf-8")   #decode bytes to utf-8 character string
             print(message)
 
-    def client_connection(self):
-        self.robot_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.robot_socket.connect((self.robot_address, self.robot_port))
-        while True:
-            #TODO This resets user input with each print... Fix it
-            time.sleep(1)
-            #message = self.robot_socket.recv(self.buffer_size)
-            #message = message.decode("utf-8")
-            #print(message)
+    def __client_connection(self):
+        '''Connect to a server socket'''
+        self.__robot_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #initialize socket
+        self.__robot_socket.connect((self.robot_address, self.__robot_port))    #Connect as a client to (remote address, port)
     
     def get_recv_buffer(self):
-        return self.robot_socket.recv(self.buffer_size)
+        '''Only get the current buffer content and return it'''
+        return self.__robot_socket.recv(self.buffer_size)
         
-    def send_input(self):
+    def _send_input(self):
         '''Take user input and send it to socket'''
         #TODO command messages should only be sent when the robot is ready to recieve new instructions
         while True:
             message = input("give input: ")   #Commands can be entered as in "popup("Hi")"
             message += "\n" #append newline to the end to avoid always typing it
-            self.robot_socket.send(bytes(message,"utf-8"))  #use the socket created in rcm to send string as bytes
+            self.__robot_socket.send(bytes(message,"utf-8"))  #use the socket created in rcm to send string as bytes
     
-    def send(self, message):
+    def send(self, message: str):
         '''take input from another module and send it'''
-        self.robot_socket.send(bytes(message,"utf-8"))  #use the socket created in rcm to send string as bytes
+        self.__robot_socket.send(bytes(message,"utf-8"))  #use the socket created in rcm to send string as bytes
 
     def begin(self, mode: str):
         '''Automatically creates needed threads so user does not need to worry about these.
         possible options for mode are: "server" and "client"'''
         if mode == "client":
-            connection_thread = threading.Thread(target=self.client_connection)
+            connection_thread = threading.Thread(target=self.__client_connection)
         elif mode == "server":
-            connection_thread = threading.Thread(target=self.server_connection)
+            connection_thread = threading.Thread(target=self.__server_connection)
         #TODO create a new mode for these, kinda annoying if input thread is always started.
         #send_thread = threading.Thread(target=self.send_input, daemon=True)
         #send_thread.start()
@@ -69,7 +67,7 @@ class Main():
         #rcm.robot_address = "192.168.100.10"    #ip of the robot, needed in client mode
         rcm.robot_address = "172.16.140.130"
         rcm.begin("client")  #start rcm in client mode
-        send_thread = threading.Thread(target=rcm.send_input, daemon=True)
+        send_thread = threading.Thread(target=rcm._send_input, daemon=True)
         send_thread.start()
         #rcm.start_server()  #start rcm in server mode
         
--- control_software/command_ui.py
+++ control_software/command_ui.py
@@ -7,12 +7,33 @@ import RobotConnectionManager
 
 class Main():
     def main(self):
+
+        ip_options = [
+            "192.168.100.10",
+            "172.16.140.130",
+            "172.16.177.128"
+        ]
+
+        port_options = [
+            "29999",
+            "30001",
+            "30020"
+        ]
+
         self.window = Tk()   #initialize the main ui windown
         self.window.title("Robot connection manager")    #give the window a title
         self.command_box = Entry(width=80)   #Text entry box to manually send commands
         self.output_box = scrolledtext.ScrolledText(self.window, wrap=WORD, width=80, height=30)
         self.output_box.grid(row=0, column=0, columnspan=5)
         self.command_box.grid(row=1, column=0, columnspan=5)   #place the commandbox to 0, 0 and say it takes x columnd
+        self.selected_ip = StringVar()
+        self.selected_port = StringVar()
+        self.selected_ip.set("192.168.100.10")
+        self.selected_port.set("30001")
+        self.port_select = OptionMenu(self.window, self.selected_port, *port_options)
+        self.ip_select = OptionMenu(self.window, self.selected_ip, *ip_options)
+        self.ip_select.grid(row=2, column=2)
+        self.port_select.grid(row=2, column=3)
         send_command_button = Button(text="send", command=lambda: Main.send_command_method(self)).grid(row=2, column=0) #create a button and place it to the grid
         start_rcm_button = Button(text="Start", command=self.start_rcm_method).grid(row=2, column=1)
         self.window.mainloop()   #start mainloop to display the window
@@ -20,21 +41,21 @@ class Main():
     def update_output_stream(self):
         self.output_box.insert(INSERT, self.rcm.get_recv_buffer())  #Insert text from the socket, or bytes as text, not formatted at all
         self.output_box.see(END)  #scrolls the "wall of text" to the bottom so most recent entry is always visible
-        #self.output_box.insert(INSERT, "text\n") #A quick test if rcm is not available
         self.window.after(1000, self.update_output_stream)  #after is a tkinter method that allows this function to call itself after a time peroid
 
     def send_command_method(self):
         command = self.command_box.get() + "\n" #get input and append newline to it for the robot to execute command as an instruction
         #self.command_box.delete(0, END) #clear command_box
-        #print(command)  #a test print
         self.rcm.send(command)  #send command using rcm objects method
         
     def start_rcm_method(self):
-        self.rcm = RobotConnectionManager.RobotConnectionManager(30001, 4096) #as in obj = Module.Class()
+        self.rcm = RobotConnectionManager.RobotConnectionManager(self.selected_ip.get(), int(self.selected_port.get()), 1024) #as in obj = Module.Class()
+        #TODO ip address is hardcoded here. fix that
         #self.rcm.robot_address = "192.168.100.10"
-        self.rcm.robot_address = "172.16.140.130"
+        #self.rcm.robot_address = "172.16.140.130"
+        #self.rcm.robot_address = "172.16.177.128"
         self.rcm.begin(mode="client")
-        self.output_box.insert(INSERT, "RCM running")
+        self.output_box.insert(INSERT, "RCM running")   #This just acts as delay so the rcm has time to start, sub optimal fix, but will do for now
         self.update_output_stream() #start a self calling function
 
 if __name__ == "__main__":
