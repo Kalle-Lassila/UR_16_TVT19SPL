d5e732c9648be39cdc9f9ec75baded88daf142c9
Kalle Lassila <kalle.lassila@protonmail.com>
2021-12-17 10:36:55 +0200

    "Final" merge

---
+++ control_software/robot_commander/key1.py
@@ -0,0 +1,10 @@
+firebaseConfig = {
+  "apiKey": "AIzaSyCJJboOE-Fa4_nPVK5u-retvOd34eAUrDw",
+  "authDomain": "ur16-dev.firebaseapp.com",
+  "databaseURL": "https://ur16-dev-default-rtdb.europe-west1.firebasedatabase.app",
+  "projectId": "ur16-dev",
+  "storageBucket": "ur16-dev.appspot.com",
+  "messagingSenderId": "959404209791",
+  "appId": "1:959404209791:web:791d21b685e0f60ca6b14a",
+  "measurementId": "G-T3C5S71BZL"
+}
